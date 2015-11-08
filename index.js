e019a68a8d483c992c18addc10a19f96ef4eaa39
Vijayee Kulkaa <vijayee.kulkaa@hushmail.com>
2015-11-08 09:58:58 -0500

    Upgraded old and busted base36 to base62 hotness. Added route 'clientprefix'

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express    = require('express');
 var bodyParser = require('body-parser');
 var levelup = require('levelup');
-var Sequence = require('base36sequence');
+var Sequence = require('base62sequence');
 
 var port = process.env.PORT || 8080;
 var db = levelup('./sequencedb');
@@ -18,6 +18,7 @@ app.use(function(err, req, res, next) {
 });
 
 var cache = {};
+var prefix;
 
 router.post('/', function(req, res) {
     //reject request with broken json fields
@@ -63,6 +64,43 @@ router.post('/', function(req, res) {
         });
     }
 
+});
+
+router.post('/clientprefix', function(req, res) {
+    var key='client-prefix';
+    if (prefix){
+        var next = prefix.next();
+        //store current sequence to db
+        db.put(key, prefix.current(), function (err, value) {
+            if (err) {
+                prefix = null;
+                return res.sendStatus(500);
+            } else {
+                res.json({prefix: next});
+            }
+        });
+    } else {
+        db.get(key, function (err, value) {
+            var next;
+            //start sequence from db
+            if (value) {
+                prefix = new Sequence(value);
+
+            } else {
+                prefix = new Sequence(0);
+            }
+            next = prefix.next();
+            //store current sequence
+            db.put(key,  prefix.current(), function (err) {
+                if (err) {
+                    prefix = null;
+                    return res.sendStatus(500);
+                } else {
+                    res.json({prefix: next});
+                }
+            });
+        });
+    }
 });
 
 app.listen(port);
